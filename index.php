79a41c020069fe4dbdfe261a46a231c47349b0b1
salvatore-impellizzeri <salvatoreimpellizzeri2001@gmail.com>
2024-10-11 11:50:23 +0200

    v 1.1

--- index.php
+++ index.php
@@ -1,228 +1,217 @@
 <?php
 
-    class Category {
-        public $name;
-        public $icon;
+class Category {
+    public $name;
+    public $icon;
 
-        function __construct(string $name, string $icon) {
-            $this->name = $name;
-            $this->icon = $icon;
-        }
+    function __construct(string $name, string $icon) {
+        $this->name = $name;
+        $this->icon = $icon;
+    }
+}
+
+class Product {
+    public $title;
+    public $price;
+    public $img;
+    protected $category;
+    
+    function __construct(string $title, float $price, string $img, Category|null $category) {
+        $this->title = $title;
+        $this->price = $price;
+        $this->img = $img;
+        $this->setCategory($category);
     }
 
-    class Product {
-        public $title;
-        public $price;
-        public $img;
-        protected $category;
-        
-        function __construct(string $title, float $price, string $img, Category|null $category) {
-            $this->title = $title;
-            $this->price = $price;
-            $this->img = $img;
-            $this->setCategory($category);
-        }
-
-        public function getCategory() {
-            return $this->category;
-        }
-
-        public function setCategory(Category|null $category) {
-            $this->category = $category;
-        }
+    public function getCategory() {
+        return $this->category;
     }
 
-    class Food extends Product {
-
-        public $ingridients;
-
-        function __construct(string $title, float $price, string $img, Category|null $category, string $ingridients) {
-            parent::__construct($title, $price, $img, $category);
-
-            $this->ingridients = $ingridients;
-        }
-
+    public function setCategory(Category|null $category) {
+        $this->category = $category;
     }
+}
 
-    class Toy extends Product {
+class Food extends Product {
+    public $ingredients;
 
-        public $materials;
-
-        function __construct(string $title, float $price, string $img, Category|null $category, string $materials) {
-            parent::__construct($title, $price, $img, $category);
-
-            $this->materials = $materials;
-        }
-
+    function __construct(string $title, float $price, string $img, Category|null $category, string $ingredients) {
+        parent::__construct($title, $price, $img, $category);
+        $this->ingredients = $ingredients;
     }
+}
 
-    class PetBed extends Product {
+class Toy extends Product {
+    public $materials;
 
-        public $size;
-
-        function __construct(string $title, float $price, string $img, Category|null $category, string $size) {
-            parent::__construct($title, $price, $img, $category);
-
-            $this->size = $size;
-        }
-
+    function __construct(string $title, float $price, string $img, Category|null $category, string $materials) {
+        parent::__construct($title, $price, $img, $category);
+        $this->materials = $materials;
     }
+}
 
-    $cani = new Category('Cani', '🐶');
-    $gatti = new Category('Gatti', '🐱');
+class PetBed extends Product {
+    public $size;
 
-    // GATTI
+    function __construct(string $title, float $price, string $img, Category|null $category, string $size) {
+        parent::__construct($title, $price, $img, $category);
+        $this->size = $size;
+    }
+}
 
-    $prodottoPerGatti = new Product(
-        'Prodotto generico per gatti', 
-        17.99, 
-        'https://www.robinsonpetshop.it/news/cms2017/wp-content/uploads/2022/07/gatto-vita.jpg',
-        $gatti,
-    );
+$cani = new Category('Cani', '🐶');
+$gatti = new Category('Gatti', '🐱');
 
-    $ciboPerGatti = new Food(
-        'Cibo per gatti',
-        22.99,
-        'https://media.dm-static.com/images/f_auto,q_auto,c_fit,h_1200,w_1200/v1702034691/products/pim/8410650157542-010380/ultima-cibo-secco-per-gatti-con-pollo-per-un-tratto-urinario-sano',
-        $gatti,
-        'Manzo, piselli, carote, sale, olio, acqua'
-    );
+// GATTI
 
-    $giocoPerGatti = new Toy(
-        'Gioco per gatti',
-        2.99,
-        '',
-        $gatti,
-        'Plastica'
-    );
+$prodottoPerGatti = new Product(
+    'Prodotto generico per gatti', 
+    17.99, 
+    'https://www.robinsonpetshop.it/news/cms2017/wp-content/uploads/2022/07/gatto-vita.jpg',
+    $gatti
+);
 
-    $cucciaPerGatti = new PetBed(
-        'Cuccia per gatti',
-        9.99,
-        '',
-        $gatti,
-        'Big'
-    );
+$ciboPerGatti = new Food(
+    'Cibo per gatti',
+    22.99,
+    'https://media.dm-static.com/images/f_auto,q_auto,c_fit,h_1200,w_1200/v1702034691/products/pim/8410650157542-010380/ultima-cibo-secco-per-gatti-con-pollo-per-un-tratto-urinario-sano',
+    $gatti,
+    'Manzo, piselli, carote, sale, olio, acqua'
+);
 
-    // CANI
+$giocoPerGatti = new Toy(
+    'Gioco per gatti',
+    2.99,
+    '',
+    $gatti,
+    'Plastica'
+);
 
-    $prodottoPerCani = new Product(
-        'Prodotto generico per cani', 
-        17.99, 
-        'https://www.b2x.it/rest/images/2023/11/15/1488336.jpg?imageFormat=@1x',
-        $cani,
-    );
+$cucciaPerGatti = new PetBed(
+    'Cuccia per gatti',
+    9.99,
+    '',
+    $gatti,
+    'Big'
+);
 
-    $ciboPerCani = new Food(
-        'Cibo per cani',
-        22.99,
-        'https://static.ultima-affinity.com/catalog/8410650216416/3d-Pack/mediumImage',
-        $cani,
-        'Manzo, piselli, carote, sale, olio, acqua'
-    );
+// CANI
 
-    $giocoPerCani = new Toy(
-        'Gioco per cani',
-        2.99,
-        'https://m.media-amazon.com/images/I/618NGaspCbL._AC_UF894,1000_QL80_.jpg',
-        $cani,
-        'Plastica'
-    );
+$prodottoPerCani = new Product(
+    'Prodotto generico per cani', 
+    17.99, 
+    'https://www.b2x.it/rest/images/2023/11/15/1488336.jpg?imageFormat=@1x',
+    $cani
+);
 
-    $cucciaPerCani = new PetBed(
-        'Cuccia per cani',
-        9.99,
-        'https://media.zooplus.com/bilder/1/400/74494_pla_cozy_kingdom_bett_fg_1931_1.jpg',
-        $cani,
-        'Big'
-    );
+$ciboPerCani = new Food(
+    'Cibo per cani',
+    22.99,
+    'https://static.ultima-affinity.com/catalog/8410650216416/3d-Pack/mediumImage',
+    $cani,
+    'Manzo, piselli, carote, sale, olio, acqua'
+);
 
-    $products = [
-        $prodottoPerGatti,
-        $ciboPerGatti,
-        $cucciaPerGatti,
-        $cucciaPerGatti,
-        $prodottoPerCani,
-        $ciboPerCani,
-        $cucciaPerCani,
-        $cucciaPerCani
-    ]
+$giocoPerCani = new Toy(
+    'Gioco per cani',
+    2.99,
+    'https://m.media-amazon.com/images/I/618NGaspCbL._AC_UF894,1000_QL80_.jpg',
+    $cani,
+    'Plastica'
+);
+
+$cucciaPerCani = new PetBed(
+    'Cuccia per cani',
+    9.99,
+    'https://media.zooplus.com/bilder/1/400/74494_pla_cozy_kingdom_bett_fg_1931_1.jpg',
+    $cani,
+    'Big'
+);
+
+$products = [
+    $prodottoPerGatti,
+    $ciboPerGatti,
+    $giocoPerGatti,
+    $cucciaPerGatti,
+    $prodottoPerCani,
+    $ciboPerCani,
+    $giocoPerCani,
+    $cucciaPerCani
+];
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Template PHP</title>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shop</title>
 
-        <link rel="stylesheet" href="./style.css">
-
-        <!-- Bootstrap -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    </head>
-    <body>
-        
-        <header class="py-4 mb-4">
-            <div class="container">
-                <div class="row">
-                    <div class="col text-center">
-                        <h1>
-                            SHOP
-                        </h1>
-                    </div>
+    <link rel="stylesheet" href="./style.css">
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    
+    <header class="py-4 mb-4">
+        <div class="container">
+            <div class="row">
+                <div class="col text-center">
+                    <h1>SHOP</h1>
                 </div>
             </div>
-        </header>
+        </div>
+    </header>
 
-        <main>
-            <div class="container">
-                <div class="row">
-                    <?php
-                        foreach ($products as $product) {
-                    ?>
-                        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-                            <div class="card">
-                                <img src="<?php $product->$img ?>" alt="">
-                                <div class="card-body">
-                                    <h2>
-                                        <?php echo $product->$title; ?>
-                                    </h2>
-                                    <h6>
-                                        <?php echo $product->getCategory()->$name?>
-                                        <?php echo $product->getCategory()->$icon?>
-                                    </h6>
-                                    <hr>
-                                    <h5>
-                                        €<?php echo number_format($product->$price, 2, ',', '.'); ?>
-                                    </h5>
-                                    <h3>
-                                        Tipo di articolo: <?php echo get_class($product);
-                                        
-                                        switch ($productClass) {
-                                            case 'Prodotto':
-                                                echo 'Generico';
+    <main>
+        <div class="container">
+            <div class="row">
+                <?php
+                    foreach ($products as $product) {
+                ?>
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                        <div class="card">
+                            <img src="<?php echo $product->img ? $product->img : 'https://via.placeholder.com/150'; ?>" alt="Immagine prodotto" class="card-img-top">
+                            <div class="card-body">
+                                <h2>
+                                    <?php echo $product->title; ?>
+                                </h2>
+                                <h6>
+                                    <?php echo $product->getCategory()->name; ?>
+                                    <?php echo $product->getCategory()->icon; ?>
+                                </h6>
+                                <hr>
+                                <h5>
+                                    €<?php echo number_format($product->price, 2, ',', '.'); ?>
+                                </h5>
+                                <h3>
+                                    Tipo di articolo: 
+                                    <?php 
+                                    $productClass = get_class($product);
+                                    switch ($productClass) {
+                                        case 'Product':
+                                            echo 'Generico';
                                             break;
-                                            case 'Food':
-                                                echo 'Cibo';
+                                        case 'Food':
+                                            echo 'Cibo';
                                             break;
-                                            case 'Toy':
-                                                echo 'Giocattoli';
+                                        case 'Toy':
+                                            echo 'Giocattoli';
                                             break;
-                                            case 'PetBed':
-                                                echo 'Cuccia';
+                                        case 'PetBed':
+                                            echo 'Cuccia';
                                             break;
-                                        }
-                                        ?>
-                                    </h3>
-                                </div>
+                                    }
+                                    ?>
+                                </h3>
                             </div>
                         </div>
-                    <?php
-                        }
-                    ?>
-                </div>
+                    </div>
+                <?php
+                    }
+                ?>
             </div>
-        </main>
+        </div>
+    </main>
 
-    </body>
+</body>
 </html>
